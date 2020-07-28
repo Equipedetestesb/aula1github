c70baf88f928aed9d8ad168da55007c019364da7
Equipedetestesb <grupodetestesb@gmail.com>
2020-07-28 14:34:06 -0300

    desafio2020

---
+++ gitAdrianaum/src/br/com/gitadrianaum/Main.java
@@ -0,0 +1,10 @@
+package br.com.gitadrianaum;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Bem-vindo ao desafio 2020");
+
+    }
+}
