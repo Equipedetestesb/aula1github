e451e024ec60259c0992349e2de4db416ffd8195
Equipedetestesb <grupodetestesb@gmail.com>
2020-07-27 23:08:48 -0300

    Projeto criado

---
+++ src/com/exemplo/helloworldadriana/HelloWorldAdriana.java
@@ -0,0 +1,7 @@
+package com.exemplo.helloworldadriana;
+
+public class HelloWorldAdriana {
+    public static void main(String[] args) {
+        System.out.println("Hello World Adriana!");
+    }
+}
